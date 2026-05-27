af23b53cc4bd6fbacc1c0951acd46e994b2bff2d
POCKETdont24 <uxsusdd@gmail.com>
2026-05-27 22:25:22 +0300

    Remove unnecessary

--- install.sh
+++ install.sh
@@ -38,62 +38,13 @@ esac
 
 #Make sure that nvim is installed
 if [! command -v "$REQUIRED_PKG" &> /dev/null]; then
+	echo "#@######################@#"
 	echo "Neovim could not be found."
-	
-	case "$MACHINE" in
-		"LINUX")
-			case "$DISTRO" in
-				arch)
-					echo "#@#############################################################@#"
-					echo "Installing on Arch linux, also install vim-plug for Neovim"
-					echo "#@#############################################################@#"
-					sudo pacman -S --noconfirm "$REQUIRED_PKG"
-					sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-					;;
-				postmarketos)
-					echo "#@###############################################################@#"
-					echo "Installing on PostMarketOS, also install vim-plug for Neovim"
-					echo "#@###############################################################@#"
-					sudo apk add "$REQUIRED_PKG"
-					sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-					;;
-				*)
-					echo "#@###############################################################@#"
-					echo "Unsupported Linux distribution,but you can install it on your own"
-					echo "Open a new terminal and install $REQUIRED_PKG"
-					read -p "Press [ENTER] once you have installed $REQUIRED_PKG to continue."
-					echo "#@###############################################################@#"
-					;;
-				#Need to add more DISTRO
-			esac
-			;;
-		"macOS")
-			echo "#@########################################################@#"
-			echo "Installing on macOS, also install vim-plug for Neovim"
-			echo "#@########################################################@#"
-			brew install "$REQUIRED_PKG"
-			sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-			;;
-		"MinGw")
-			echo "#@##########################################################@#"
-			echo "Installing on Windows, also install vim-plug for Neovim"
-			echo "#@##########################################################@#"
-			winget install "$REQUIRED_PKG"
-			powershell.exe -Command "iwr -useb https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim | ni \"(\$num = \${env:XDG_DATA_HOME}; if(\$num){ \$num } else { \${env:LOCALAPPDATA} })/nvim-data/site/autoload/plug.vim\" -Force"
-			;;
-		*)
-			echo "#@###################@#"
-			echo "Unsupported on $MACHINE"
-			echo "#@###################@#"
-			;;
-	esac
+	echo "#@######################@#"
 else
-	echo "#@##################################################################@#"
+	echo "#@####################################################@#"
 	echo "Neovim is installed, need to install vim-plug for Neovim"
-	echo "#@##################################################################@#"
+	echo "#@####################################################@#"
 
 	case "$MACHINE" in 
 		"LINUX"|"macOS")
